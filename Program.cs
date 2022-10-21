9cd7776f0bf4eee4f39f2ea2a58afba380349e16
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2022-10-21 10:25:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace uppgift_4._24
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                    continue;
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                    continue;
+                }
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FuzzBuzz");
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
