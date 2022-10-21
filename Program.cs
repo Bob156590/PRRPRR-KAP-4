0bc40f0a1af37a7dd9955d569f5fee55858eb8ef
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2022-10-21 10:47:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uppgift_4._25
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("write a number pls.");
+            string n = Console.ReadLine();
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                string s = n[i].ToString();
+                int num = int.Parse(s);
+                int num2 = (num + 1);
+                Console.Write(num2);
+            }
+        }
+    }
+}
