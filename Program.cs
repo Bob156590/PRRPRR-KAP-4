f7603d266beeeaff776f9e96ef5e7c02e59d50e1
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2022-10-23 16:28:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+
+namespace uppgift_4._22
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i <= 30; i++)  
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            for (int i = 200; 180 <= i; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            for (int i = 1000; i <= 1400; i += 50)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
