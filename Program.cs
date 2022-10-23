cc11b63b9ea283a4af674f33d8a4ccbca097630a
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2022-10-23 14:49:13 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace uppgift_4._27
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pls write a messige.");
+            string m = Console.ReadLine();
+            Console.WriteLine("Pls write how many spaces to the right do you want you messege to be.");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < m.Length; i++)
+            {
+                
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(m[i]);
+            }
+        }
+    }
+}
