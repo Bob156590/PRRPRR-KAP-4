cd15fa27cdb50c31d44ec65ae1400fa95c478cef
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2022-10-23 15:46:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace uppgift_4._28
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pls the hight for the triangel.");
+            int h = int.Parse(Console.ReadLine());
+            int j = 0;
+            for (int i = 0; i < h; i++)
+            {
+                do
+                {
+                    Console.Write("*");
+                    j++;
+                } while (j <= i);
+                Console.WriteLine();
+                j = 0;
+            }
+        }
+    }
+}
