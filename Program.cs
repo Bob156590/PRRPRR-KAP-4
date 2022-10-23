a540fad855ab6aa702cb0126dd5af37ad900c969
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2022-10-23 15:10:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace uppgift_4._28
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pls write a sentens.");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Pls write some latters and the program will find if you used any of the letter in the first letter.");
+            string str2 = Console.ReadLine();
+            bool flag = false;
+            str1.ToLower();
+            str2.ToLower();
+            for (int i = 0; i < str1.Length; i++)
+            {
+                if (str2[i] == str1[i])
+                {
+                    flag = true;
+                }
+
+                if (flag)
+                {
+                    Console.WriteLine("You used some of the letters.");
+                }
+                else
+                {
+                    Console.WriteLine("You didn't use any of the letters.");
+                }
+
+            }
+        }
+    }
+}
