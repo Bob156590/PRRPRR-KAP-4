9f3d8b647438ea4a76c6280f95ebb5591e8fa9cb
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2022-10-28 11:18:13 +0200

    Update uppgift 28.cs

--- uppgift 28.cs
+++ uppgift 28.cs
@@ -33,3 +33,38 @@ namespace uppgift_4._28
         }
     }
 }
+
+
+using System;
+
+namespace uppgift_4._28
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pls write a sentens.");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Pls write some two charcacters.");
+            string str2 = Console.ReadLine();
+            char n1 = str2.ToCharArray()[0];
+            char n2 = str2.ToCharArray()[1];
+            bool flag = false;
+            foreach (char i in str1)
+            {
+                if (i == n1 && i == n2)
+                {
+                    flag = true;
+                }
+            }
+            if (flag)
+            {
+                Console.WriteLine("You used some of the letters.");
+            }
+            else
+            {
+                Console.WriteLine("You didn't use any of the letters.");
+            }
+        }
+    }
+}
